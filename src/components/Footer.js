bb7cec653aa3be1a1c6b70e1e6dfcbd8ad339bdb
HEECHANG96 <wjdgmlckd96@daum.net>
2023-05-15 12:40:09 +0900

    react router 기본 세팅 및 footer css 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer>
       <div>
-        <div>개인정보 처리방침 | 이용 약관</div>
-        <div>All rights reserved @ Codestates</div>
+        <div className="footer-letter">개인정보 처리방침 | 이용 약관</div>
+        <div className="footer-letter">All rights reserved @ Codestates</div>
       </div>
-    </div>
+    </footer>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,48 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="header">
       <div className="logo-title-section">
         <img
           className="logo"
-          src="https://s3-alpha-sig.figma.com/img/a646/01d8/d9897ad884e6380838afc832f8a97155?Expires=1684713600&Signature=eqV6s8pgzqLHg7nNUeBRA2TcSDPzXEbuOWyPrAWGWD5fomOBkd19hfx2tm5Be8C23Jzgn1qtiCE1LC5VDlR6OpfXx481wKjOY9z6oEm7PYTEGL5q0ymM0mQxU3VDZ6seCkB1~PKZDk8KLgjHPKmnwnTRMbA2YN~HC90fcjcEoU1gK4BgvLIWwcSyaOpWGyj9M~c0O5WtCxsUzDzq6cRRyPw4DQ5P6UY4UpPPEZQfLPAZvlqi8wTKL0CIk5kVgc7e~9UTx03nkqCV1GFMSrZQmf~egHylS8kgHr--wIZNbfgzce32M-etRkJDX2IhWOZvsG7mlbfhwTFmgGKhfGBENw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          src="../images/logo.png"
+          onClick={() => {
+            navigate("/");
+          }}
+          alt=""
         />
         <h1 className="title">COZ Shopping</h1>
       </div>
-      <FontAwesomeIcon icon={faBars} className="faBars" />
+      <div className="hamburger-menu-section">
+        <FontAwesomeIcon icon={faBars} className="faBars" />
+        <div className="dropdown">
+          <div className="menu-list radius">정희창님, 안녕하세요!</div>
+          <div
+            className="menu-list radius"
+            onClick={() => {
+              navigate("/products/list");
+            }}
+          >
+            <img src="../images/product.png" alt="" />
+            상품리스트
+          </div>
+          <div
+            className="menu-list"
+            onClick={() => {
+              navigate("/bookmark");
+            }}
+          >
+            <img src="../images/bookmark.png" alt="" />
+            북마크 페이지
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
